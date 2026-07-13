e6524735c05a4c954e8cf763002828decbae8e68
Jeeyhosting <abidmustafamuhammad@gmail.com>
2026-07-14 02:33:39 +0800

    Delete models directory

--- models/Setting.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const mongoose = require('mongoose');
-
-const schema = new mongoose.Schema({
-  key:   { type: String, required: true, unique: true },
-  value: { type: String, required: true },
-}, { timestamps: { createdAt: false, updatedAt: 'updated_at' } });
-
-module.exports = mongoose.model('Setting', schema);
